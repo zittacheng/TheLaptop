9b4a59d56a1c54027c4297acfe2ce3a006175038
Zittacheng <zittacheng@gmail.com>
2019-10-02 12:45:46 -0400

    3

---
+++ Assets/Script/ButtonCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LAP
+{
+    public class ButtonCreator : MonoBehaviour {
+        public GameObject Prefab;
+        public float Count;
+        public Vector3 StartPosition;
+        public float Distance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Process()
+        {
+            for (int i = transform.childCount - 1; i > 0; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+            Vector3 a = StartPosition + transform.position;
+            for (int i = 1; i < Count; i++)
+            {
+                a += new Vector3(Distance, 0, 0);
+                GameObject G = Instantiate(Prefab,transform);
+                G.transform.position = a;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Editor/Editor_ButtonCreate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace LAP
+{
+    [CustomEditor(typeof(ButtonCreator))]
+    [CanEditMultipleObjects]
+    public class Editor_ButtonCreate : Editor {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            ButtonCreator BC = (ButtonCreator)target;
+            if (GUILayout.Button("ASD"))
+                BC.Process();
+        }
+    }
+}
